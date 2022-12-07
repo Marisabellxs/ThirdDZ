890f6e9faf3350894de5e8a2cbdbce255adc4567
Marisabell <mary_tusa@mail.ru>
2022-12-07 22:52:19 +0400

    Решение

--- Задача 21/Program.cs
+++ Задача 21/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите координаты Х числа А");
+double Xa = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите координаты Y числа А");
+double Ya = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите координаты Z числа A");
+double Za = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите координаты Х числа B");
+double Xb = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите координаты Y числа B");
+double Yb = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите координаты Z числа B");
+double Zb = int.Parse(Console.ReadLine());
+double AB = Math.Pow(Xb-Xa, 2)+Math.Pow(Yb-Ya, 2)+Math.Pow(Zb-Za, 2);
+AB = Math.Sqrt(AB);
+Console.WriteLine(AB);
