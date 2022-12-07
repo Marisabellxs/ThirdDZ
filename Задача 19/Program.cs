9e2376f59b9b58e12d9f1bcacaee1b4345c7aec5
Marisabell <mary_tusa@mail.ru>
2022-12-07 23:34:26 +0400

    Решение

--- Задача 19/Program.cs
+++ Задача 19/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите пятизначное число");
+string number = Console.ReadLine()!;
+int length = number.Length;
+if (length==5)
+{
+    if (number[0]==number[4] && number[1]==number[3])
+    {
+        Console.WriteLine("Да");
+    }
+    else
+    {
+        Console.WriteLine("Нет");
+    }
+}
+else
+{
+    Console.WriteLine("Не пятизначное число");
+}
